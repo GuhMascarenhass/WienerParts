4c8082ba462da47da5cada69215022db5671b270
GuhMascarenhass <gustavo_santos2548@hotmail.com>
2025-06-18 10:31:39 -0300

    Add Flask, tailwind. Recode de css and code base

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route("/")
+def base():
+    return render_template("base.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug= True, host="0.0.0.0")
