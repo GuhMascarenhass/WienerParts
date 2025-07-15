4cc26ec6e6d8aeff1aa32ad2b7c3575e039ac8d2
GuhMascarenhass <gustavo_santos2548@hotmail.com>
2025-07-14 22:19:12 -0300

    Getting back the information from database, code to show the data on db

--- peca/views.py
+++ peca/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, make_response
+from flask import render_template, request, make_response, jsonify
 
 from peca import app, db
 from peca.models import Product
@@ -6,23 +6,24 @@ from peca.models import Product
 
 @app.route("/", methods=["GET", "POST"])
 def base():
- 
+    dados = Product.query.all()
     if request.method == "GET":
         print("")
     if request.method == "POST":
-        name = request.form['name']
-        type = request.form['tipo']
-        produtos = Product(name=name, tipo= type)
+        name = request.form["name"]
+        type = request.form["tipo"]
+        produtos = Product(name=name, tipo=type)
 
         db.session.add(produtos)
         db.session.commit()
 
-    return render_template("base.html")
+    return render_template("base.html", dados=dados)
 
 
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    dados = db.Query.all()
+    return render_template("index.html", dados=dados)
 
 
 def select_dados():
