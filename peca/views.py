f4971e561a90e8456f8f096eab7ee6b141729e03
GuhMascarenhass <gustavo_santos2548@hotmail.com>
2025-07-22 22:34:24 -0300

    Add Delete function

--- peca/views.py
+++ peca/views.py
@@ -1,13 +1,19 @@
-from flask import render_template, request, make_response, jsonify, flash
-
+from flask import render_template, request, flash, redirect
 from peca import app, db
 from peca.models import Product, Form_product
 
 
-@app.route("/", methods=["GET", "POST"])
-def base():
-    dados = Product.query.all()
+@app.route("/")
+def home():
     form = Form_product(request.form)
+    dados_banco = Product.query.all()
+    return render_template("base.html", dados=dados_banco, form=form)
+
+
+@app.route("/enviar", methods=["GET", "POST"])
+def create_product():
+    form = Form_product(request.form)
+
     if request.method == "GET":
         print("")
     if request.method == "POST" and form.validate():
@@ -16,14 +22,17 @@ def base():
         flash("Peca salva com sucesso!!")
         db.session.commit()
 
-    return render_template("base.html", dados=dados, form=form)
+    return redirect("/")
 
 
-@app.route("/index")
-def index():
-    dados = db.Query.all()
-    return render_template("index.html", dados=dados)
-
-
-def select_dados():
+@app.route("/update", methods=["POST"])
+def update_product():
     return
+
+
+@app.route("/delete/<int:product_id>", methods=["POST"])
+def delete_product(product_id):
+    id_delete = Product.query.get(product_id)
+    db.session.delete(id_delete)
+    db.session.commit()
+    return redirect("/")
